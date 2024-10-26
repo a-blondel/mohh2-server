952c7a2a353aa595b3d13071776ffdc5b43d8e46
a-blondel <ablondel@protonmail.com>
2024-10-26 21:01:06 +0200

    Exclude deleted personas from leaderboards

--- src/main/java/com/ea/repositories/PersonaStatsRepository.java
+++ src/main/java/com/ea/repositories/PersonaStatsRepository.java
@@ -11,19 +11,21 @@ import java.util.List;
 public interface PersonaStatsRepository extends JpaRepository<PersonaStatsEntity, Long> {
 
     PersonaStatsEntity findByPersonaIdAndVers(Long id, String vers);
+
     PersonaStatsEntity findByPersonaIdAndVersIn(Long id, List<String> vers);
+
     @Query(value = "SELECT RANK FROM " +
             "(SELECT PERSONA_ID, ROW_NUMBER() OVER(ORDER BY (KILL - DEATH) DESC, PERSONA_ID ASC) AS RANK " +
             "FROM PERSONA_STATS PS " +
             "JOIN PERSONA P ON PS.PERSONA_ID = P.ID " +
-            "WHERE PS.VERS = ?2 AND PS.PLAYTIME > 0) AS STATS " +
+            "WHERE PS.VERS = ?2 AND PS.PLAYTIME > 0 AND P.DELETED_ON IS NULL) AS STATS " +
             "WHERE STATS.PERSONA_ID = ?1", nativeQuery = true)
     Long getRankByPersonaIdAndVers(long id, String vers);
 
-    @Query(value = "FROM PersonaStatsEntity ps WHERE ps.vers = :vers AND ps.playTime > 0 ORDER BY (kill - death) DESC, persona.id ASC LIMIT :limit OFFSET :offset")
+    @Query(value = "FROM PersonaStatsEntity ps WHERE ps.vers = :vers AND ps.playTime > 0 AND ps.persona.deletedOn IS NULL ORDER BY (kill - death) DESC, persona.id ASC LIMIT :limit OFFSET :offset")
     List<PersonaStatsEntity> getLeaderboardByVers(String vers, long limit, long offset);
 
-    @Query(value = "FROM PersonaStatsEntity ps WHERE ps.vers = :vers AND ps.playTime > 0 ORDER BY kill DESC, persona.id ASC LIMIT :limit OFFSET :offset")
+    @Query(value = "FROM PersonaStatsEntity ps WHERE ps.vers = :vers AND ps.playTime > 0 AND ps.persona.deletedOn IS NULL ORDER BY kill DESC, persona.id ASC LIMIT :limit OFFSET :offset")
     List<PersonaStatsEntity> getWeaponLeaderboardByVers(String vers, long limit, long offset);
 
 }
