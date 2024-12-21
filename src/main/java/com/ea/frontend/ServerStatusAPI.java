a8fc73b3bdf6ec3ca526212eb868f067c80decca
a-blondel <ablondel@protonmail.com>
2024-12-21 23:51:22 +0100

    Add password icon to gamelist

--- src/main/java/com/ea/frontend/ServerStatusAPI.java
+++ src/main/java/com/ea/frontend/ServerStatusAPI.java
@@ -53,6 +53,7 @@ public class ServerStatusAPI
                 game.version(),
                 MapMoHH.getMapNameByHexId(game.params().split(",")[1]),
                 game.params(),
+                game.pass(),
                 api.toUTCInstant(game.startTime()),
                 getMaxPlayerSize(game.maxPlayers()),
                 game.hostName(),
